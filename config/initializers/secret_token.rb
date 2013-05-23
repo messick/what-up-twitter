9f9e3880380427db9240923ed9308cd2c4fefd03
Nick Messick <nmessick@gmail.com>
2013-05-22 21:31:55 -0600

    rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WhatUpTwitter::Application.config.secret_token = 'e64aaa9eb70f09f5f5a5aac3d8eaa7eb4651fc9196b74d73de02296034b1052977b1bf3c18b4c0bfde3874c45d5fb118195860b2ab77b4c1b1096b95cf674549'
