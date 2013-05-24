b6f3349704fb4e6e8017fe81c1d5a0bf5581460e
Nick Messick <nmessick@gmail.com>
2013-05-23 23:04:56 -0600

    minor user model changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
   # :lockable, :timeoutable and :omniauthable
   devise :omniauthable
 
-  attr_accessible :email, :password, :password_confirmation, :remember_me
+  attr_accessible :email, :password
 
   def self.from_omniauth(auth)
     where(auth.slice(:provider, :uid)).first_or_create do |user|
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,14 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe '#mass-assignment' do
+    it { should allow_mass_assignment_of :email }
+    it { should allow_mass_assignment_of :password }
+    it { should_not allow_mass_assignment_of :provider }
+    it { should_not allow_mass_assignment_of :uid }
+    it { should_not allow_mass_assignment_of :user_name }
+    it { should_not allow_mass_assignment_of :secret }
+    it { should_not allow_mass_assignment_of :token }
+  end
 end
