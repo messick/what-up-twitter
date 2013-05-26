c84bb9a2712b3ef2b2a5b51f575d2390f2a2b401
Nick Messick <nmessick@gmail.com>
2013-05-25 23:45:47 -0600

    remove unneeded file

--- spec/factories/searches.rb
+++
@@ -1,8 +0,0 @@
-# Read about factories at https://github.com/thoughtbot/factory_girl
-
-FactoryGirl.define do
-  factory :search do
-    term "MyString"
-    last_cached_at "2013-05-23 22:05:57"
-  end
-end
